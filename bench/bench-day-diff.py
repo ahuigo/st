a65c6c222ee73bef10f19fda99eff437482f1615
ahuigo <1781999+ahuigo@users.noreply.github.com>
2023-03-03 03:08:07 +0800

    prieces

--- bench/bench-day-diff.py
+++ bench/bench-day-diff.py
@@ -4,17 +4,20 @@ from db.conn import getProApi
 from datetime import datetime,timedelta
 from collections import deque
 
-from bench.prices import prices
-maxTransactionAmount = 8*10**4
+#from bench.prices import prices
+maxTransactionAmount = 2*10**4
+deltaDiff = 0.001
+# todo: buy with diff-0.01(high,low)
 
 pro = getProApi()
 # print("pro....")
 # data = pro.daily(ts_code='000001.SZ', start_date='20180701', end_date='20180718')
 #data = getProApi().stock_basic(exchange='', list_status='L', fields='ts_code,symbol,name,area,industry,list_date')
 codes = [
-#'300750.SZ',
+'300059.SZ', # 东方 0.020
+#'600036.SH', # 0.016
+#'300750.SZ', # 宁德
 #'300498.SZ',
-'300059.SZ',
 # '300124.SZ',
 # '300769.SZ',
 # '300763.SZ',
@@ -23,11 +26,12 @@ codes = [
 # '300760.SZ',
 # '300896.SZ',
 ]
-if False:
-    import json
-    prices = priceDb.getPullPricesByCode(codes[0],1, 400)
-    prices = [{"trade_date": str(p['trade_date']), "close":p['close']} for p in prices]
-    print(json.dumps(prices)); quit()
+import json
+prices = priceDb.getPullPricesByCode(codes[0],1, 500)
+if 'high' not in prices[-1]:
+    prices.pop()
+prices = [{"trade_date": str(p['trade_date']), "close":p['close'], "high":p['high'],"low":p['low']} for p in prices]
+#print(json.dumps(prices)); quit()
 
 
 
@@ -58,6 +62,10 @@ def iterReduce(arr, return_end=False):
                 yield old_item, None
             break
         yield old_item,item
+
+def iterWindow2(seq, n=2):
+    for i in range(len(seq) - n + 1):
+        yield seq[i: i + n]
             
 def calcYj(p):
     r = p*0.00025
@@ -92,7 +100,7 @@ def buySt(price, diff):
     global mysts,balance,trade_num
     if balance>maxTransactionAmount:
         trade_num += 1
-        amount = math.ceil(maxTransactionAmount/price/100)
+        amount = math.ceil(maxTransactionAmount/(price*100))
         transactionAmount = amount*100*price
         balance -= amount*100*price*(1+0.0001)+calcYj(transactionAmount)
         print("amount=",amount*100)
@@ -123,22 +131,41 @@ def printSts(price):
 
 
 minBalance =balance 
-for p1,p2 in iterReduce(iter(prices)):
+import sys
+if len(sys.argv)>=2:
+    triggerDiff = float(sys.argv[1])
+else:
+    triggerDiff = 0.02
+
+#def benchDiff( prices, ):
+for p1,p2,p3 in iterWindow2(prices,3):
     if minBalance>balance:
         minBalance =balance 
     tdate, pc1, pc2= p2['trade_date'],p1['close'],p2['close']
+    low,high = p3['low'],p3['high']
     diff = pc2/pc1-1
     print("")
     print(tdate, pc2, '{:.1f}'.format(diff*100))
     if diff>0.01: # not important
-        print("sell:", end="")
-        sellSt(pc2)
-    elif diff< -0.020:
-        print("buy:",end="")
-        buySt(pc2, -diff)
+        price = round(pc2*(1+deltaDiff), 2)
+        price = pc2
+        high = pc2
+        if price <=high:
+            #if price < low: price = low-0
+            print("sell:", end="")
+            sellSt(price)
+    elif diff< -triggerDiff: # < 0.02
+        price = round(pc2*(1-deltaDiff),2)
+        price = pc2
+        low = pc2
+        if price>=low:
+            #if price > high: price = high+0
+            print("buy:",end="")
+            buySt(price, -diff)
     if abs(diff)>0.01:
         printSts(pc2)
     else:
         print("total:", total(pc2))
 
-print("total end:", total(prices[-1]['close']), ",trade_num:", trade_num, ",minBalance:", minBalance)
+print("totalend:", total(prices[-1]['close']), ",trade_num:", trade_num, ",minBalance:", minBalance)
+print("tirggerDiff", triggerDiff)
